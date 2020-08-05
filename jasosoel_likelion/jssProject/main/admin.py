ec9819b6eb15633f4894a57df013625b7f46d4ed
thalals <thalsal@naver.com>
2020-08-05 19:15:04 +0900

    자소설 닷컴 2(index.html, create.html)

--- jasosoel_likelion/jssProject/main/admin.py
+++ jasosoel_likelion/jssProject/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Jasoseol
 # Register your models here.
+
+#admin 페이지 내가만든 모델을 보기위헤 추가
+admin.site.register(Jasoseol)
--- jasosoel_likelion/jssProject/main/models.py
+++ jasosoel_likelion/jssProject/main/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Jasoseol(models.Model):
-    title = models.CharField(Max_Length=50)
+    title = models.CharField(max_length=50)
     content = models.TextField()
-    update_at = models.models.DateTimeField(auto_now = True)
+    update_at = models.DateTimeField(auto_now = True)
     #날짜와 시간을 받음, auto_now = true 는 업데이트될때 날짜 자동저장
+
+    # def __str__(self):
+    #     return self.name
