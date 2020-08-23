0bf528fe0634577b9b4d38d849739554e4db2458
thalals <thalsal@naver.com>
2020-08-23 17:16:33 +0900

    asdf

--- jssProject/settings.py
+++ jssProject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEFUG',False))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG',False))
 
 ALLOWED_HOSTS = ['*']
 
